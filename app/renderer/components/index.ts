ef4278717899e793bb2ecf8d415416d8d8fec97a
LionTech <tykodev@outlook.com>
2025-08-09 18:49:36 +0200

    chore: remove screenshots and fix run-agents lint

---
+++ app/renderer/components/index.ts
@@ -0,0 +1,2 @@
+export { default as CustomTag } from './CustomTag'
+export { default as DividerWithText } from './DividerWithText'
---
+++ app/routes/files.tsx
@@ -0,0 +1,8 @@
+export default function Files() {
+  return (
+    <main className="p-4">
+      <h1>Files</h1>
+      <p>File management coming soon.</p>
+    </main>
+  )
+}
